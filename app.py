43c0a76b93770258880003e3db154f2607649599
Shuchir Jain <53063247+ShuchirJ@users.noreply.github.com>
2026-03-09 21:55:17 +0000

    add templating

--- app.py
+++ app.py
@@ -6,34 +6,8 @@ messages = []
 
 @app.route("/")
 def home():
-    return render_template("index.html", messages=messages)
+    return render_template("index.html")
 
-@app.post("/")
-def accept_message():
-    message = request.form['message']
-    messages.append(message)
-    return redirect("/") # sends the browser to the GET route
-
-@app.post("/delete")
-def delete_message():
-   message = request.form['message']
-   messages.remove(message)
-   return redirect("/")
-
-"""
-Add a delete button next to each list item,
-The button should be in a form next to a hidden input that has the message to remove,
-The delete button should go to a python route that removes that 
-message from the list and redirects to /
-"""
-
-
-
-"""
-Make a website that takes messages from users using a text input 
-and submit button. The website should show all messages people 
-have submitted. You can use a list to store messages received, 
-then send the list to the HTML to show each message. 
-Here is a quick tutorial: 
-https://github.com/ShuchirJ/flask-lesson/blob/main/templating.md
-"""
+@app.route("/images")
+def images():
+    return render_template("images.html")
